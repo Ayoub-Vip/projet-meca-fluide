c81ac4451bfaf3dcbb99cc7e0b61e60ba7c7887a
Ayoub ASSAOUD <86426829+Ayoub-Vip@users.noreply.github.com>
2022-04-01 11:17:30 +0200

    Update code.py

--- code.py
+++ code.py
@@ -6,6 +6,7 @@ Created on Thu Mar 24 16:21:01 2022
 
 import numpy as np
 
+
 def getCoeff(num_left,num_right,num_down,num_up,num_cent,type_cent,cl_cent):
     if type_cent == 0:
         return 0
@@ -17,8 +18,8 @@ def getCoeff(num_left,num_right,num_down,num_up,num_cent,type_cent,cl_cent):
        
     
     if type_cent == 1:
-        a = np.array([1,1,1,1,-4], ndmin = 2).T
+        a = np.array([[1],[1],[1],[1],[-4]])
         b = 0
-        j = np.array([num_left, num_right, num_down, num_up, num_cent], ndmin = 2).T
+        j = np.array([[num_left], [num_right], [num_down], [num_up], [num_cent]])
     
     return j,a,b
